aa0919caec1d94bccfd9d47a8de3b66a4ec071a1
drubio687 <drubio1@hawk.iit.edu>
2017-03-03 11:57:06 -0600

    Added js code for scroll menu

---
+++ home.js
@@ -0,0 +1,22 @@
+//added no conflict for jQuery
+$.noConflict();
+
+/* function for menu */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
