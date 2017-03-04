99c28d3ba993346e943c8d08ae2a4ed56300f575
drubio687 <drubio1@hawk.iit.edu>
2017-03-03 19:42:37 -0600

    Fixed javascript to work with webpage

--- home.js
+++ home.js
@@ -1,22 +1,62 @@
-//added no conflict for jQuery
-$.noConflict();
-
-/* function for menu */
-function scrlFunc() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function validate()
+{
+if(document.form.name.value=='')
+{
+alert("Please enter your name");
+document.form.name.focus();
+return false;
 }
+else
+if(document.form.alias.value=='')
+{
+alert("Please Enter Alias.");
+document.form.alias.focus();
+return false;
+}
+else
+if(document.form.password.value=='')
+{
+alert("Please Enter Password.");
+document.form.password.focus();
+return false;
+}
+else
+if(document.form.password.value.length<6)
+{
+alert("Password Must Be Greater than 6 characters.");
+document.form.password.focus();
+return false;
+}
+else
+if(document.form.confm.value=='')
+{
+alert("Password don't match, please try again.");
+document.form.confm.focus();
+return false;
+}
+else
+if(document.form.password.value!=document.form.confm.value)
+{
+alert("Please Enter correct Re type Password");
+document.form.confm.focus();
+return false;
+}
+else
+if(document.form.email.value=='')
+{
+alert("Please Enter E-Mail.");
+document.form.email.focus();
+return false;
+}
+else
+var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 
+if(!document.form.email.value.match(emailExp))
+     {
+alert('Invalid Email');
+document.form.email.focus();
+return false;
+     }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+return true;
 }
